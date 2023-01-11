5c1f3a3fa5e0c76a9ee9c9aa8727d39214f6e45b
fiacre <armeldion95@gmail.com>
2023-01-11 02:56:21 +0000

    first commit

---
+++ Day3/DAILY_CHALLENGE_GOLD/script.js
@@ -0,0 +1,8 @@
+const numbers = [5,0,9,1,7,4,2,6,3,8];
+
+let ConversionTostring = numbers.toString();
+console.log(ConversionTostring);
+
+let ConversionJoin = numbers.join(' + ');
+console.log(ConversionJoin);
+
